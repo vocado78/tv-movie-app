d8bb9c83fce41b73299bb7ccfc4ddcd6c0ed8674
vocado78 <hedeliningrid@gmail.com>
2019-11-06 13:06:58 -0800

    sets up routing and puts together homescreen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import '../../index.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import HomeScreen from '../../screens/HomeScreen/HomeScreen';
 
 
 export default function App() {
   return (
-    <div className="text-4xl text-center text-red-500">App</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={HomeScreen} />
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
