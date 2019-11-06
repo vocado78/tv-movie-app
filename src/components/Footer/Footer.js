1355539daf2eeedfea364d0033ee59096894ff83
vocado78 <hedeliningrid@gmail.com>
2019-11-05 20:26:38 -0800

    adds footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Footer() {
+  return (
+    <footer className="h-40 bg-gray-200 text-gray-500 text-center">
+      Made with &#127861 by vocado78.
+    </footer>
+  );
+}
