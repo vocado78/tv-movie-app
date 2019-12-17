a37dc8e2d5d1beeaa0fc038de0e0d62c718c42e5
vocado78 <hedeliningrid@gmail.com>
2019-12-17 12:40:35 -0800

    capitalises profile folder

---
+++ src/components/Profile/ProfileTabs/Tabs/Discussions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ProfileTab from './ProfileTab';
+
+
+export default function Discussions() {
+  return (
+    <ProfileTab title="This is the discussions title">
+      <p>No discussions joined yet.</p>
+    </ProfileTab>
+  );
+}
