236ceeea3e77da434b368b624c301edd058ce6ee
vocado78 <hedeliningrid@gmail.com>
2019-12-31 19:31:10 -0800

    fixes dropdown title last child border right

--- src/components/BrowseBar/Dropdown/Dropdown.js
+++ src/components/BrowseBar/Dropdown/Dropdown.js
@@ -9,22 +9,20 @@ export default function Dropdown({ title, items, onClick }) {
   const dropdownContentClass = 'hidden absolute -ml-4 mt-1 bg-gray-400 border-t border-solid border-gray-600 text-gray-600 capitalize rounded-b py-1 px-3 w-32 dropdown-content';
 
   return (
-    <ul>
-      <DropdownTitle title={title}>
-        <ul className={dropdownContentClass}>
-          {items.map(({ id, label }) => {
-            return (
-              <DropdownItem
-                key={id}
-                id={label}
-                label={label}
-                onClick={onClick}
-              />
-            );
-          })}
-        </ul>
-      </DropdownTitle>
-    </ul>
+    <DropdownTitle title={title}>
+      <ul className={dropdownContentClass}>
+        {items.map(({ id, label }) => {
+          return (
+            <DropdownItem
+              key={id}
+              id={label}
+              label={label}
+              onClick={onClick}
+            />
+          );
+        })}
+      </ul>
+    </DropdownTitle>
   );
 }
 
--- src/components/BrowseBar/Dropdown/DropdownTitle/DropdownTitle.js
+++ src/components/BrowseBar/Dropdown/DropdownTitle/DropdownTitle.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 import { ReactComponent as ArrowDownIcon } from './arrow-down.svg';
 
 export default function DropdownTitle({ title, children }) {
-  const itemClass = 'dropdown-container w-32 px-4 py-1 border-r border-solid border-gray-600 relative hover:text-white hover:bg-gray-400 hover:rounded-t';
+  const titleClass = 'dropdown-container w-32 px-4 py-1 border-r border-solid border-gray-600 last:border-r-0 relative hover:text-white hover:bg-gray-400 hover:rounded-t';
 
   return (
-    <li className={itemClass}>
+    <li className={titleClass}>
       <p className="tracking-wider inline">{title}</p>
       <ArrowDownIcon className="inline ml-2 fill-current" />
       {children}
@@ -19,5 +19,3 @@ DropdownTitle.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired
 };
-
-// last:border-r-0
