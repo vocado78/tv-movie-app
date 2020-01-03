ab676d4a1239bc7668852a995c61cd781d3a47fc
vocado78 <hedeliningrid@gmail.com>
2020-01-02 20:48:15 -0800

    hooks searchresults up to store

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 
-export default function SearchResults() {
+function SearchResults(props) {
   const params = useParams();
-  console.log(params);
+  console.log(params, props.genre, props.decade);
+
+  // compare genre and decade from params with genre and decade in store
+  // if not the same, update store with genre and/or decade from params
 
   return (
     <section>
@@ -13,3 +18,17 @@ export default function SearchResults() {
     </section>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    genre: state.dropdowns.activeGenre,
+    decade: state.dropdowns.activeDecade
+  };
+};
+
+export default connect(mapStateToProps)(SearchResults);
+
+SearchResults.propTypes = {
+  genre: PropTypes.string.isRequired,
+  decade: PropTypes.string.isRequired
+};
