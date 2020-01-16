87379d31e2e9a0495d5a8c81f9f6d2686388d116
vocado78 <hedeliningrid@gmail.com>
2020-01-15 20:38:05 -0800

    updates snapshots

--- src/components/BrowseBar/BrowseBar.stories.js
+++ src/components/BrowseBar/BrowseBar.stories.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import BrowserBar from './BrowseBar';
 
+const store = {
+  getState: () => {
+    return {
+      dropdowns: {
+        activeGenre: 'genre',
+        activeDecade: 'decade'
+      },
+    };
+  },
+  subscribe: () => 0,
+  dispatch: () => {}
+};
 
 export default {
   component: BrowserBar,
   title: 'BrowserBar',
   decorators: [
-    (story) => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+    (story) => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>,
+    (story) => <Provider store={store}>{story()}</Provider>
   ]
 };
 
