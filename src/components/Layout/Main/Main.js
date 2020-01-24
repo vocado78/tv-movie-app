55546e33e47a3de957ca245e24c7b7504811a870
vocado78 <hedeliningrid@gmail.com>
2020-01-24 10:21:18 -0800

    extracts Main css to tw css file

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -3,13 +3,8 @@ import PropTypes from 'prop-types';
 
 
 export default function Main({ children }) {
-  const mainBackground = {
-    backgroundColor: '#14181c',
-    backgroundImage: 'linear-gradient(180deg, #1d242c, #13171c)'
-  };
-
   return (
-    <div className="py-8" style={mainBackground}>
+    <div className="main-background">
       {children}
     </div>
   );
